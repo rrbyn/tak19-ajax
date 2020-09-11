8e2435660f835ed177d163b37ed0f96c229419ea
rrbyn <robin.kukk23@gmail.com>
2020-09-11 14:14:19 +0300

    bruh bruh bruh

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+const bookIdInput = document.getElementById('book-id')
+
+bookIdInput.addEventListener('change', e => {
+    fetchBookData(e.currentTarget.value)
+})
+
+
+function fetchBookData(bookId) {
+    fetch('http://localhost:8080/book.php?id=' + bookId)
+        .then(res => {
+            return res.json()
+        })
+        .then(book => {
+            console.log(book)
+        })
+}
